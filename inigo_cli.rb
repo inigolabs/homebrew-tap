e82a6e65ac42a4381aee664a91c98352fe4e1bfd
goreleaserbot <goreleaser@carlosbecker.com>
2022-08-09 09:38:35 -0700

    Brew formula update for cli version v0.13.0

--- inigo_cli.rb
+++ inigo_cli.rb
@@ -5,20 +5,20 @@
 class InigoCli < Formula
   desc "Inigo CLI"
   homepage "https://www.inigolabs.com"
-  version "0.13"
+  version "0.13.0"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/inigolabs/cli/releases/download/v0.13/cli_macos_arm64.tar.gz"
-      sha256 "857d608c7fee1269eb11abbfd66b8a9ca83fffa2673a407e21e3237249b04772"
+      url "https://github.com/inigolabs/cli/releases/download/v0.13.0/cli_macos_arm64.tar.gz"
+      sha256 "7b6288bd85d0ddc7cf60762b9635ff7bc289b9e7fc3f0dd1533e839f180fd057"
 
       def install
         bin.install "inigo"
       end
     end
     if Hardware::CPU.intel?
-      url "https://github.com/inigolabs/cli/releases/download/v0.13/cli_macos_x86_64.tar.gz"
-      sha256 "4ee1ad79b227dde58115a5582eb1168a885da7ea0ff77d0082e4f15fe93c6db8"
+      url "https://github.com/inigolabs/cli/releases/download/v0.13.0/cli_macos_x86_64.tar.gz"
+      sha256 "05dc571ccffd26827936f241d33b8341e71614eaa2eb63903a7b84d031421398"
 
       def install
         bin.install "inigo"
@@ -28,16 +28,16 @@ class InigoCli < Formula
 
   on_linux do
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url "https://github.com/inigolabs/cli/releases/download/v0.13/cli_linux_arm64.tar.gz"
-      sha256 "a8349cd9afbe4dc281efda921d48f690cc7baa29069c6628d19d1be1cceb6ec6"
+      url "https://github.com/inigolabs/cli/releases/download/v0.13.0/cli_linux_arm64.tar.gz"
+      sha256 "640133e2f02f5c9516658058e2274d78a52205ad30f93fd701ed2523973ba1c7"
 
       def install
         bin.install "inigo"
       end
     end
     if Hardware::CPU.intel?
-      url "https://github.com/inigolabs/cli/releases/download/v0.13/cli_linux_x86_64.tar.gz"
-      sha256 "8429d192e2537db47e29b96af337b06a03a9d8595cedffec6136cf4c48a85856"
+      url "https://github.com/inigolabs/cli/releases/download/v0.13.0/cli_linux_x86_64.tar.gz"
+      sha256 "83cd1767e24e9e79ee304cd736ecb231f07f55441c076d85f3997dd38ddfd761"
 
       def install
         bin.install "inigo"
